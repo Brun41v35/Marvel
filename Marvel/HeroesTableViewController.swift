241c8fbf69d2c5ee055e28e339f42cb6c121c053
Bruno Alves <alves.bruno945@gmail.com>
2020-12-22 11:47:16 -0300

    Adicionando pods

---
+++ Marvel/HeroesTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HeroesTableViewController.swift
+//  Marvel
+//
+//  Created by Bruno Silva on 22/12/20.
+//
+
+import UIKit
+
+class HeroesTableViewController: UITableViewController {
+    
+    // MARK: - Life Cycle View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
